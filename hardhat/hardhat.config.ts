4684ad46922c9716300c6525a7204997a050fe2a
Dafu <datnp100394@gmail.com>
2022-04-04 11:42:00 +0700

    add hardhat package

---
+++ hardhat/hardhat.config.ts
@@ -0,0 +1,48 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.0",
+  networks: {
+    testnets: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      accounts:
+        ["49bce5e20035523a95acd664d28f9f6f7a2e1c677fb8161e14ec30ec9dac871a"],
+      chainId: 97,
+      gasPrice: 20000000000,
+    },
+  },
+
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
+};
+
+export default config;
